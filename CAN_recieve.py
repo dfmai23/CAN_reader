b25ac9cf4d1b8e76e7910af41bf774423aa056ab
DFM <dfmai@ucdavis.edu>
2017-05-11 05:42:31 -0700

    implemented file logging

--- CAN_recieve.py
+++ CAN_recieve.py
@@ -2,8 +2,11 @@ import serial
 import threading
 import os
 from tkinter import *
+from datetime import datetime as dt
 
 from CAN_globals import CAN_map, msg, new_msg, UNKNOWN
+
+logs_path = os.getcwd() + '\\logs\\'
 
 class CAN_recieve(threading.Thread):
     def __init__(self, srl):                         #constructor
@@ -19,30 +22,37 @@ class CAN_recieve(threading.Thread):
 
         size = 0
 
+        if not os.path.exists(logs_path):           #make logs folder
+            os.makedirs(logs_path)
+        datetime = dt.now().strftime('(%Y-%m-%d)_%H.%M.%S')   
+        #log = open(logs_path+datetime+'.txt', 'w')
+        log = 0
+        #print(logs_path+datetime)
+    
         while self.alive:
             try:
                 #msg = srl.read(16) #acquire the wireless CAN message r
                 msg = self.srl.readline()        #'\n' delimiter
                 size = len(msg)
 
-                #print(msg)
                 if size >= 16 and msg[size-2] == 255:   #0xFF0A delimiter
-                    self.CAN_process(msg, size)
+                    self.CAN_process(msg, size, log)
                 #if size >=16 and msg[size-2] == 255:
                 else:   #get rest of message
                     msg += self.srl.readline()  
                     size = len(msg)
                     if size >= 16 and msg[size-2] == 255:   #0xFF0A delimiter
-                        self.CAN_process(msg, size)
+                        self.CAN_process(msg, size, log)
                 #else
             #try
             except KeyboardInterrupt:
                 self.alive = False                 #close this thread
             #except
         #while
+        #file.close() 
     #run(self)
 
-    def CAN_process(self, msg, size):
+    def CAN_process(self, msg, size, log):
         mask1 = 0
         mask2 = 0
         mask3 = 0
@@ -54,12 +64,12 @@ class CAN_recieve(threading.Thread):
         mark4 = msg[size-11]
         timestamp = mask1 | mask2 | mask3 | mask4
 
-        #print(msg)
+        #print('%u %s' % (size, msg))
         new_msg=('0x%01X%02X     %02X %02X %02X %02X %02X %02X %02X %02X     %d.%03d' %
         (msg[size-16], msg[size-15],
         msg[size-10], msg[size-9], msg[size-8], msg[size-7], msg[size-6], msg[size-5], msg[size-4], msg[size-3],
         timestamp // 1000, timestamp % 1000))
-        print(new_msg)
+        #print(new_msg)
        
         CAN_ID = ('%1X%02X' % (msg[size-16], msg[size-15]))
         CAN_msg = CAN_map.get(CAN_ID)
@@ -69,4 +79,7 @@ class CAN_recieve(threading.Thread):
             UNKNOWN.set(new_msg)
 
         #log to file
+        #log.write('%s,%u,%X,%X,%X,%X,%X,%X,%X,%X\n' % 
+        #(CAN_ID, timestamp, msg[size-10], msg[size-9], msg[size-8], msg[size-7], msg[size-6], msg[size-5], msg[size-4], msg[size-3]))
+    #CAN_process()
 #end class
